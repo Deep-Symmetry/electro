d5bb6d4958ad9c071d101ee59f857e3da589e248
James Elliott <>
2018-08-03 15:33:28 -0500

    Fleshing out the implementation.

--- src/main/java/org/deepsymmetry/electro/Metronome.java
+++ src/main/java/org/deepsymmetry/electro/Metronome.java
@@ -46,6 +46,113 @@ public class Metronome {
     }
 
     /**
+     * Restarts the metronome at the beginning of the specified beat number.
+     *
+     * @param beat the beat to which the metronome should jump; the first beat is beat 1
+     */
+    public void jumpToBeat(long beat) {
+
+    }
+
+    /**
+     * Restarts the metronome at the start of the specified bar, keeping the beat phase unchanged in case
+     * it is being synchronized to an external source.
+     *
+     * @param bar the bar to which the metronome should jump; the first bar is bar 1
+     */
+    public void jumpToBar(long bar) {
+
+    }
+
+    /**
+     * Restarts the metronome at the start of the specified phrase, keaping the beat phase unchanged in case
+     * it is being synchronized to an external source.
+     *
+     * @param phrase the phrase to which the metronome should jump; the first phrase is phrase 1
+     */
+    public void jumpToPhrase(long phrase) {
+
+    }
+
+    /**
+     * Adds a number of milliseconds to the start time of the metronome. Useful to nudge it back into synchronization
+     * with an external source.
+     *
+     * @param ms the number of milliseconds to add to the start time
+     */
+    public synchronized void adjustStart(long ms) {
+        startTime.addAndGet(ms);
+    }
+
+    /**
+     * Get the tempo at which this metronome is running.
+     *
+     * @return the number of beats per minute being counted
+     */
+    public double getTempo() {
+        return tempo.get();
+    }
+
+    /**
+     * Establish a new tempo for the metronome. The start time will be adjusted so that the current beat and phase
+     * are unaffected by the tempo change.
+     *
+     * @param tempo the number of beats per minute at which the metronome should now run
+     */
+    public synchronized void setTempo(double tempo) {
+        this.tempo.set(tempo);
+        // TODO Adjust start!
+    }
+
+    /**
+     * Get the number of beats per bar in the metronome's beat grid. The default value is four.
+     *
+     * @return the number of beats per bar being counted
+     */
+    public int getBeatsPerBar() {
+        return beatsPerBar.get();
+    }
+
+    /**
+     * Establish a new number of beats per bar in the metronome's beat grid.
+     *
+     * @param beatsPerBar a positive number of beats per bar being counted
+     *
+     * @throws IllegalArgumentException if {@code beatsPerBar} is not greater than zero
+     */
+    public void setBeatsPerBar(int beatsPerBar) {
+        if (beatsPerBar > 0) {
+            this.beatsPerBar.set(beatsPerBar);
+        } else {
+            throw new IllegalArgumentException("beatsPerBar must be greater than zero");
+        }
+    }
+
+    /**
+     * Get the number of bars per phrase in the metronome's beat grid.
+     *
+     * @return the number of bars per phrase being counted
+     */
+    public int getBarsPerPhrase() {
+        return barsPerPhrase.get();
+    }
+
+    /**
+     * Establish a new number of bars per phrase in the metronome's beat grid.
+     *
+     * @param barsPerPhrase a positive number of bars per phrase being counted
+     *
+     * @throws IllegalArgumentException if {@code barsPerPhrase} is not greater than zero
+     */
+    public void setBarsPerPhrase(int barsPerPhrase) {
+        if (barsPerPhrase > 0) {
+            this.barsPerPhrase.set(barsPerPhrase);
+        } else {
+            throw new IllegalArgumentException("barsPerPhrase must be greater than zero");
+        }
+    }
+
+    /**
      * Calculate the number of milliseconds taken by the specified number of beats at the specified tempo.
      *
      * @param beats the number of beats to time
@@ -53,7 +160,7 @@ public class Metronome {
      *
      * @return the number of milliseconds that would pass while that many beats play at that tempo
      */
-    public long beatsToMilliseconds(long beats, double tempo) {
+    public static long beatsToMilliseconds(long beats, double tempo) {
         return Math.round((60000.0 / tempo) * beats);
     }
 
@@ -68,7 +175,7 @@ public class Metronome {
      *
      * @return the marker number currently in effect for the metronome at the specified instant
      */
-    public long markerNumber(long instant, long start, long interval) {
+    public static long markerNumber(long instant, long start, long interval) {
         return ((instant - start) / interval) + 1;
     }
 
@@ -85,21 +192,199 @@ public class Metronome {
      *
      * @return the phase in effect for the specified marker at the specified instant, in the range [0.0, 1.0)
      */
-    public double markerPhase(long instant, long start, long interval) {
+    public static double markerPhase(long instant, long start, long interval) {
         final double ratio = (instant - start) / (double)interval;
         return ratio - Math.floor(ratio);
     }
 
     /**
-     * Ensure that a phase falls in the range [0.0, 1.0).
+     * Ensure that a phase falls in the range [0.0, 1.0). Values outside the range will have their non-fractional
+     * part discarded.
      *
      * @return the normalized phase.
      */
-    public double normalizePhase(double phase) {
+    public static double normalizePhase(double phase) {
         if (phase < 0.0) {
             return (phase - (long)phase) + 1.0;
         }
         return phase - (long)phase;
+    }
+
+    /**
+     * Get the number of milliseconds a beat lasts given the current tempo.
+     *
+     * @return the duration of a beat
+     */
+    public synchronized long getBeatInterval() {
+        return beatsToMilliseconds(1, tempo.get());
+    }
+
+    /**
+     * Get the number of milliseconds a bar lasts given the current configuration and tempo.
+     *
+     * @return the duration of a bar
+     */
+    public synchronized long getBarInterval() {
+        return beatsToMilliseconds(beatsPerBar.get(), tempo.get());
+    }
+
+    /**
+     * Get the number of milliseconds a phrase lasts given the current configuration and tempo.
+     *
+     * @return the duration of a phrase
+     */
+    public synchronized long getPhraseInterval() {
+        return beatsToMilliseconds(beatsPerBar.get() * barsPerPhrase.get(), tempo.get());
+    }
+
+    /**
+     * Get the current beat being played.
+     *
+     * @return the current beat number, which starts at 1
+     */
+    public synchronized long getBeat() {
+
+    }
+
+    /**
+     * Determine the millisecond timestamp at which a particular beat will occur.
+     *
+     * @param beat the number of the beat whose start time is desired
+     *
+     * @return the time at which the specified beat begins
+     */
+    public synchronized long getTimeOfBeat(long beat) {
+
+    }
+
+    /**
+     * Determine the distance traveled into the current beat as a phase number in the range [0.0, 1.0).
+     *
+     * @return the current beat phase
+     */
+    public synchronized double getBeatPhase() {
+
+    }
+
+    /**
+     * Nudge the metronome so that it has reached the specified part of its current beat. If the value supplied is
+     * outside the range of a beat phase (less than zero or greater than or equal to one), it will be normalized to
+     * fit into that range by ignoring the non-fractional part.
+     *
+     * @param phase the desired beat phase, in the range [0.0, 1.0).
+     */
+    public synchronized void setBeatPhase(double phase) {
+
+    }
+
+    /**
+     * Get the current bar being played.
+     *
+     * @return the current bar number, which starts at 1
+     */
+    public synchronized long getBar() {
+
+    }
+
+    /**
+     * Determine the millisecond timestamp at which a particular bar will occur.
+     *
+     * @param bar the number of the bar whose start time is desired
+     *
+     * @return the time at which the specified bar begins
+     */
+    public synchronized long getTimeOfBar(long bar) {
+
+    }
+
+    /**
+     * Determine the distance traveled into the current bar as a phase number in the range [0.0, 1.0).
+     *
+     * @return the current bar phase
+     */
+    public synchronized double getBarPhase() {
+
+    }
+
+    /**
+     * Nudge the metronome so that it has reached the specified part of its current bar. If the value supplied is
+     * outside the range of a bar phase (less than zero or greater than or equal to one), it will be normalized to
+     * fit into that range by ignoring the non-fractional part.
+     *
+     * @param phase the desired bar phase, in the range [0.0, 1.0).
+     */
+    public synchronized void setBarPhase(double phase) {
+
+    }
+
+    /**
+     * Get the current phrase being played.
+     *
+     * @return the current phrase number, which starts at 1
+     */
+    public synchronized long getPhrase() {
+
+    }
+
+    /**
+     * Determine the millisecond timestamp at which a particular phrase will occur.
+     *
+     * @param phrase the number of the phrase whose start time is desired
+     *
+     * @return the time at which the specified phrase begins
+     */
+    public synchronized long getTimeOfPhrase(long phrase) {
+
+    }
+
+    /**
+     * Determine the distance traveled into the current phrase as a phase number in the range [0.0, 1.0).
+     *
+     * @return the current phrase phase
+     */
+    public synchronized double getPhrasePhase() {
+
+    }
+
+    /**
+     * Nudge the metronome so that it has reached the specified part of its current phrase. If the value supplied is
+     * outside the range of a phrase phase (less than zero or greater than or equal to one), it will be normalized to
+     * fit into that range by ignoring the non-fractional part.
+     *
+     * @param phase the desired phrase phase, in the range [0.0, 1.0).
+     */
+    public synchronized void setPhrasePhase(double phase) {
+
+    }
+
+    /**
+     * Take a snapshot of the current beat, bar, phrase, and phase state, so coherent calculations about them can be
+     * performed with respect to a static point in time.
+     *
+     * @return a representation of the detailed metronome state at the current moment
+     */
+    public synchronized Snapshot getSnapshot() {
+        return new Snapshot(this);
+    }
+
+    /**
+     * Take a snapshot of the beat, bar, phrase, and phase state that the metronome would have at the specified
+     * millisecond timestamp, so coherent calculations about them can be performed with respect to that static point
+     * in time.
+     *
+     * @return a representation of the detailed metronome state at the specified moment
+     */
+    public synchronized Snapshot getSnapshot(long instant) {
+        return new Snapshot(this, instant);
+    }
+
+    /**
+     * Returns the current count of the metronome as "phrase.bar.beat".
+     *
+     * @return a concise textual representation of the current metronome position
+     */
+    public String getMarker () {
+        return getSnapshot().getMarker();
     }
 
 
--- src/main/java/org/deepsymmetry/electro/Snapshot.java
+++ src/main/java/org/deepsymmetry/electro/Snapshot.java
@@ -4,8 +4,9 @@ package org.deepsymmetry.electro;
  * A snapshot to support a series of beat and phase calculations with respect to a given instant in time,
  * so the calculations can all remain consistent even if they take a while to complete.
  *
- * Snapshots also extend the notions of beat phase to enable oscillators with frequencies that are fractions or
- * multiples of a beat.
+ * Snapshots in Afterglow also extend the notions of beat phase to enable oscillators with frequencies that are
+ * fractions or multiples of a beat. Since that is much more difficult in Java than Clojure, and unlikely to be
+ * needed outside the context of a lighting controller, it has not yet been ported. Open an issue if you need it!
  *
  * @author James Elliott
  */
@@ -40,17 +41,17 @@ public class Snapshot {
     /**
      * The beat number being played at the time represented by the snapshot. Beats start at 1.
      */
-    public final int beat;
+    public final long beat;
 
     /**
      * The bar number being played at the time represented by the snapshot. Bars start at 1.
      */
-    public final int bar;
+    public final long bar;
 
     /**
      * The phrase number being played at the time represented by the snapshot. Phrases start at 1.
      */
-    public final int phrase;
+    public final long phrase;
 
     /**
      * The metronome's beat phase at the time of the snapshot, a value which starts at 0.0 at the very start of the
@@ -70,6 +71,92 @@ public class Snapshot {
      */
     public final double phrasePhase;
 
+    /**
+     * Create a snapshot of the state of the metronome at the current instant in time.
+     *
+     * @param metronome the time keeper whose current state is to be analyzed and frozen
+     */
+    Snapshot(Metronome metronome) {
+        this(metronome, System.currentTimeMillis());
+    }
 
+    /**
+     * Create a snapshot of the state of the metronome at a particular instant in time.
+     *
+     * @param metronome the time keeper whose current state is to be analyzed and frozen
+     * @param instant the millisecond timestamp at which the state of the metronome is to be considered
+     */
+    Snapshot (Metronome metronome, long instant) {
+        startTime = metronome.getStartTime();
+        tempo = metronome.getTempo();
+        beatsPerBar = metronome.getBeatsPerBar();
+        barsPerPhrase = metronome.getBarsPerPhrase();
+        this.instant = instant;
+        beat = Metronome.markerNumber(instant, startTime, metronome.getBeatInterval());
+        bar = Metronome.markerNumber(instant, startTime, metronome.getBarInterval());
+        phrase = Metronome.markerNumber(instant, startTime, metronome.getPhraseInterval());
+        beatPhase = Metronome.markerPhase(instant, startTime, metronome.getBeatInterval());
+        barPhase = Metronome.markerPhase(instant, startTime, metronome.getBarInterval());
+        phrasePhase = Metronome.markerPhase(instant, startTime, metronome.getPhraseInterval());
+    }
 
+    /**
+     * Return the beat number of the snapshot relative to the start of the bar: the down beat is 1, and the range
+     * goes up to the value of {@link #beatsPerBar}.
+     *
+     * @return the beat number within the current bar being counted
+     */
+    public int getBeatWithinBar() {
+        final double beatSize = 1.0 / beatsPerBar;
+        return 1 + (int)Math.floor(barPhase / beatSize);
+    }
+
+    /**
+     * Checks whether the current beat at the time of the snapshot was the first beat in its bar.
+     *
+     * @return {@code true} if the snapshot was taken during the first beat of a bar
+     */
+    public boolean isDownBeat() {
+        return getBeatWithinBar() == 1;
+    }
+
+    /**
+     * Return the beat number of the snapshot relative to the start of the phrase: the phrase starts with beat 1, and
+     * the range goes up to the value of {@link #beatsPerBar} times {@link #barsPerPhrase}.
+     *
+     * @return the beat number within the current phrase being counted
+     */
+    public int getBeatWithinPhrase() {
+        final double beatSize = 1.0 / (beatsPerBar * barsPerPhrase);
+        return 1 + (int)Math.floor(phrasePhase / beatSize);
+    }
+
+    /**
+     * Checks whether the current beat at the time of the snapshot was the first beat in its phrase.
+     *
+     * @return {@code true} if the snapshot was taken during the first beat of a phrase
+     */
+    public boolean isPhraseStart() {
+        return getBeatWithinPhrase() == 1;
+    }
+
+    /**
+     * Return the bar number of the snapshot relative to the start of the phrase: the phrase starts with bar 1, and the
+     * range goes up to the value of {@link #barsPerPhrase}.
+     *
+     * @return the bar number within the current phrase being counted
+     */
+    public int getBarWithinPhrase() {
+        final double barSize = 1.0 / barsPerPhrase;
+        return 1 + (int)Math.floor(phrasePhase / barSize);
+    }
+
+    /**
+     * Returns the time represented by the snapshot as "phrase.bar.beat".
+     *
+     * @return a concise textual representation of the current metronome position at the time of the snapshot
+     */
+    public String getMarker () {
+        return phrase + "." + getBarWithinPhrase() + "." + getBeatWithinBar();
+    }
 }
